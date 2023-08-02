f0c719ba8ec297d06e82c48db920fd5e591cb6d7
Doridian <git@doridian.net>
2023-08-01 22:18:46 -0700

    strip down CRC

--- data/parse.py
+++ data/parse.py
@@ -1,33 +1,8 @@
-def reflect_data(x, width):
-    # See: https://stackoverflow.com/a/20918545
-    if width == 8:
-        x = ((x & 0x55) << 1) | ((x & 0xAA) >> 1)
-        x = ((x & 0x33) << 2) | ((x & 0xCC) >> 2)
-        x = ((x & 0x0F) << 4) | ((x & 0xF0) >> 4)
-    elif width == 16:
-        x = ((x & 0x5555) << 1) | ((x & 0xAAAA) >> 1)
-        x = ((x & 0x3333) << 2) | ((x & 0xCCCC) >> 2)
-        x = ((x & 0x0F0F) << 4) | ((x & 0xF0F0) >> 4)
-        x = ((x & 0x00FF) << 8) | ((x & 0xFF00) >> 8)
-    elif width == 32:
-        x = ((x & 0x55555555) << 1) | ((x & 0xAAAAAAAA) >> 1)
-        x = ((x & 0x33333333) << 2) | ((x & 0xCCCCCCCC) >> 2)
-        x = ((x & 0x0F0F0F0F) << 4) | ((x & 0xF0F0F0F0) >> 4)
-        x = ((x & 0x00FF00FF) << 8) | ((x & 0xFF00FF00) >> 8)
-        x = ((x & 0x0000FFFF) << 16) | ((x & 0xFFFF0000) >> 16)
-    else:
-        raise ValueError('Unsupported width')
-    return x
-
-def crc_poly(data, n, poly, crc=0, ref_in=False, ref_out=False, xor_out=0):
+def crc_poly(data, n, poly, crc=0):
     g = 1 << n | poly  # Generator polynomial
 
     # Loop over the data
     for d in data:
-        # Reverse the input byte if the flag is true
-        if ref_in:
-            d = reflect_data(d, 8)
-
         # XOR the top byte in the CRC with the input byte
         crc ^= d << (n - 8)
 
@@ -40,12 +15,8 @@ def crc_poly(data, n, poly, crc=0, ref_in=False, ref_out=False, xor_out=0):
             if crc & (1 << n):
                 crc ^= g
 
-    # Reverse the output if the flag is true
-    if ref_out:
-        crc = reflect_data(crc, n)
-
     # Return the CRC value
-    return crc ^ xor_out
+    return crc
 
 #  It uses a standard CRC8 function, with a seed (initial value) of 0x00, and 0x85 for the polynomial.
 def n64_checksum(data):
