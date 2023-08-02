3360954461a547f5f06a9fe1bd7f70a59b9b18d7
Doridian <git@doridian.net>
2023-08-02 01:31:01 -0700

    rename

--- include/joybus_pio.hpp
+++ include/joybus_pio.hpp
@@ -19,7 +19,7 @@ typedef struct JoybusControllerInfo {
     uint8_t aux;
 } JoybusControllerInfo;
 
-#define UINT16_FIX(v) (((v >> 8) & 0x00FF) | ((v << 8) & 0xFF00))
+#define UINT16_FIX_ENDIAN(v) (((v >> 8) & 0x00FF) | ((v << 8) & 0xFF00))
 
 JoybusPIOInstance joybus_pio_program_init(PIO pio, uint sm, uint pin);
 int joybus_pio_transmit_receive(JoybusPIOInstance instance, uint8_t payload[], int payload_len, uint8_t response[], int response_len);
--- src/joybus_pio.cpp
+++ src/joybus_pio.cpp
@@ -106,6 +106,6 @@ JoybusControllerInfo joybus_init(JoybusPIOInstance instance, bool reset) {
   info.type = 0x0000;
   uint8_t payload[] = { reset ? (uint8_t)0xFF : (uint8_t)0x00 };
   joybus_pio_transmit_receive(instance, payload, 1, (uint8_t*)&info, 3);
-  info.type = UINT16_FIX(info.type);
+  info.type = UINT16_FIX_ENDIAN(info.type);
   return info;
 }
