627e9ae268fcbe63c72946cc8a71d623b9b490f8
Doridian <git@doridian.net>
2023-08-02 20:43:55 -0700

    turn state reading into inline util

--- src/joybus_gamecube.cpp
+++ src/joybus_gamecube.cpp
@@ -4,41 +4,27 @@
 #include "joybus_gamecube.hpp"
 #include "joybus_pio.hpp"
 
-GCControllerState joybus_gc_short_poll(JoybusPIOInstance instance, uint8_t flags) {
+static inline GCControllerState joybus_gc_read_state(JoybusPIOInstance instance, uint8_t payload[], int payload_len, int response_overhead) {
   GCControllerState state;
-  uint8_t payload[] = { 0x40, 0x03, flags };
-  int res = joybus_pio_transmit_receive(instance, payload, 3, (uint8_t*)&state, 8);
-  if (res != 8) {
-    Serial.println("WRONG LEN");
-    Serial.println(res);
+  int res = joybus_pio_transmit_receive(instance, payload, payload_len, (uint8_t*)&state, 9 + response_overhead);
+  if (res != 8 + response_overhead) {
     state.buttons = 0x0;
   }
   state.buttons = UINT16_FIX_ENDIAN(state.buttons);
   return state;
+}
+
+GCControllerState joybus_gc_short_poll(JoybusPIOInstance instance, uint8_t flags) {
+  uint8_t payload[] = { 0x40, 0x03, flags };
+  return joybus_gc_read_state(instance, payload, 3, 0);
 }
 
 GCControllerState joybus_gc_probe_origin(JoybusPIOInstance instance) {
-  GCControllerState state;
   uint8_t payload[] = { 0x41 };
-  int res = joybus_pio_transmit_receive(instance, payload, 1, (uint8_t*)&state, 10);
-  if (res != 10) {
-    Serial.println("WRONG LEN");
-    Serial.println(res);
-    state.buttons = 0x0;
-  }
-  state.buttons = UINT16_FIX_ENDIAN(state.buttons);
-  return state;
+  return joybus_gc_read_state(instance, payload, 1, 2);
 }
 
 GCControllerState joybus_gc_recalibrate(JoybusPIOInstance instance) {
-  GCControllerState state;
   uint8_t payload[] = { 0x42, 0x00, 0x00 };
-  int res = joybus_pio_transmit_receive(instance, payload, 3, (uint8_t*)&state, 10);
-  if (res != 10) {
-    Serial.println("WRONG LEN");
-    Serial.println(res);
-    state.buttons = 0x0;
-  }
-  state.buttons = UINT16_FIX_ENDIAN(state.buttons);
-  return state;
+  return joybus_gc_read_state(instance, payload, 3, 2);
 }
