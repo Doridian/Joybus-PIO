485353f8002c63ed678d2ab15c1017d4e0114966
Doridian <git@doridian.net>
2023-08-03 21:12:44 -0700

    cleanup GC read

--- src/joybus_gamecube.cpp
+++ src/joybus_gamecube.cpp
@@ -4,14 +4,17 @@
 #include "joybus_gamecube.hpp"
 #include "joybus_pio.hpp"
 
+#define GC_STATE_NO_OVERHEAD (8)
+#define GC_STATE_2B_OVERHEAD (GC_STATE_NO_OVERHEAD+2)
+
 static inline GCControllerState joybus_gc_read_state(JoybusPIOInstance instance,
                                                      uint8_t payload[],
                                                      int payload_len,
-                                                     int response_overhead) {
+                                                     int response_len) {
   GCControllerState state;
   state.valid = joybus_pio_transmit_receive(
                     instance, payload, payload_len, (uint8_t *)&state,
-                    8 + response_overhead) == 8 + response_overhead;
+                    response_len) == response_len;
   state.buttons = UINT16_FIX_ENDIAN(state.buttons);
   return state;
 }
@@ -19,17 +22,17 @@ static inline GCControllerState joybus_gc_read_state(JoybusPIOInstance instance,
 GCControllerState joybus_gc_short_poll(JoybusPIOInstance instance,
                                        uint8_t flags) {
   uint8_t payload[] = {0x40, 0x03, flags};
-  return joybus_gc_read_state(instance, payload, 3, 0);
+  return joybus_gc_read_state(instance, payload, 3, GC_STATE_NO_OVERHEAD);
 }
 
 GCControllerState joybus_gc_probe_origin(JoybusPIOInstance instance) {
   uint8_t payload[] = {0x41};
-  return joybus_gc_read_state(instance, payload, 1, 2);
+  return joybus_gc_read_state(instance, payload, 1, GC_STATE_2B_OVERHEAD);
 }
 
 GCControllerState joybus_gc_recalibrate(JoybusPIOInstance instance) {
   uint8_t payload[] = {0x42, 0x00, 0x00};
-  return joybus_gc_read_state(instance, payload, 3, 2);
+  return joybus_gc_read_state(instance, payload, 3, GC_STATE_2B_OVERHEAD);
 }
 
 #define GC_NORMALIZE_ELEMENT(NAME)                                             \
