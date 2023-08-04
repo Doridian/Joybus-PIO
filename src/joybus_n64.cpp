5fe352215877f46bf8de53fbc5a58a478398cd55
Doridian <git@doridian.net>
2023-08-03 22:10:00 -0700

    Add checksum into util file

--- src/joybus_n64.cpp
+++ src/joybus_n64.cpp
@@ -3,41 +3,11 @@
 
 #include "joybus_n64.hpp"
 #include "joybus_pio.hpp"
-
-static uint8_t address_xor_table[] = {0x01, 0x1A, 0x0D, 0x1C, 0x0E, 0x07,
-                                      0x19, 0x16, 0x0B, 0x1F, 0x15};
-
-static uint32_t make_address_checksummed(uint address) {
-  uint32_t rpos = address << 5; // 32?
-  for (int i = 15; i >= 5; i--) {
-    if (rpos & (0b1 << i)) {
-      rpos ^= address_xor_table[15 - i];
-    }
-  }
-  return rpos;
-}
-
-static uint8_t make_data_checksum(uint8_t data[], int len) {
-  uint32_t generator = 0x185;
-  uint32_t crc = 0x00;
-
-  for (int i = 0; i < len; i++) {
-    crc ^= data[i]; // << (n - 8) with n = 8
-
-    for (int b = 0; b < 8; b++) {
-      crc <<= 1;
-      if (crc & (0b1 << 8)) {
-        crc ^= generator;
-      }
-    }
-  }
-
-  return crc & 0xFF;
-}
+#include "joybus_checksum.hpp"
 
 int joybus_n64_read_memory(JoybusPIOInstance instance, uint address,
                            uint8_t response[]) {
-  uint32_t address_checksummed = make_address_checksummed(address);
+  uint32_t address_checksummed = joybus_checksummed_address(address);
   uint8_t payload[3] = {0x02, (uint8_t)(address_checksummed >> 8),
                         (uint8_t)(address_checksummed & 0xFF)};
   uint8_t raw_response[N64_BLOCK_SIZE + 1];
@@ -49,7 +19,7 @@ int joybus_n64_read_memory(JoybusPIOInstance instance, uint address,
     }
     return -20;
   }
-  if (make_data_checksum(response, N64_BLOCK_SIZE) !=
+  if (joybus_data_checksum(response, N64_BLOCK_SIZE) !=
       response[N64_BLOCK_SIZE]) {
     return -30;
   }
@@ -59,12 +29,12 @@ int joybus_n64_read_memory(JoybusPIOInstance instance, uint address,
 
 int joybus_n64_write_memory(JoybusPIOInstance instance, uint address,
                             uint8_t data[]) {
-  uint32_t address_checksummed = make_address_checksummed(address);
+  uint32_t address_checksummed = joybus_checksummed_address(address);
   uint8_t payload[N64_BLOCK_SIZE + 3] = {0x03,
                                          (uint8_t)(address_checksummed >> 8),
                                          (uint8_t)(address_checksummed & 0xFF)};
   memcpy(payload + 3, data, N64_BLOCK_SIZE);
-  uint8_t checksum = make_data_checksum(data, N64_BLOCK_SIZE);
+  uint8_t checksum = joybus_data_checksum(data, N64_BLOCK_SIZE);
   uint8_t response;
   int result = joybus_pio_transmit_receive(instance, payload,
                                            N64_BLOCK_SIZE + 3, &response, 1);
